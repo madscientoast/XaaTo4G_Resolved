06dcf05d2a01d2217e67180823fa6107462445fa
Robert Snuggs <robertpsnuggsjr@gmail.com>
2025-09-01 19:42:02 -0400

    Add files via upload

---
+++ Limits/JSON/ScaleLimits2.py
@@ -0,0 +1,29 @@
+import json
+
+def CalculateSigma(N,L,eff):
+    return (N)/(L*sum(eff))
+
+def ScaleDict(m,s_obs,s_scale):
+    for n in m:
+        m[n] = m[n] * s_obs / s_scale
+
+# Set input/output filenames and scaling factor
+input_file = "limits.json"
+output_file = "scaled_limits.json"
+sigma = {'300.0': CalculateSigma(3*5.5,1.1,[0.06,0.27,0.33]),'3000.0':CalculateSigma(3*1,1.1,[0.19,0.4,0.43])}
+scale = 1.0 #fb^-1 (Set the relative theory scale for the plot)
+
+# Step 1: Load the JSON file
+with open(input_file, "r") as f:
+    data = json.load(f)
+
+for mass in data:
+    ScaleDict(data[mass],sigma[mass],scale)
+print(data) #Inspect to ensure this looks right for your scale
+
+
+# Step 3: Write to a new JSON file
+with open(output_file, "w") as f:
+    json.dump(data, f, indent=2)
+
+print(f"Scaled data saved to {output_file}")
